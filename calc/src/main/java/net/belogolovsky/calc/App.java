3b039fed986d740d5ec1c53c1e1d0d6fed7e3f9d
Igor Belogolovsky <igor@belogolovsky.net>
2020-10-29 00:13:50 +0300

    Create Project

---
+++ calc/src/main/java/net/belogolovsky/calc/App.java
@@ -0,0 +1,13 @@
+package net.belogolovsky.calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
