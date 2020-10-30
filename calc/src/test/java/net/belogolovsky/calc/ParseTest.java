0652b7873dda5702cddcf641d8cb1ea3c7cfb39a
Igor Belogolovsky <igor@belogolovsky.net>
2020-10-30 15:47:53 +0300

    Tests added

--- calc/src/test/java/net/belogolovsky/calc/ParseTest.java
+++ calc/src/test/java/net/belogolovsky/calc/ParseTest.java
@@ -25,4 +25,52 @@ public class ParseTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testTwoMinusTwo() {
+        Parse parse;
+        try {
+            parse = new Parse("2 - 2");
+            int op1 = parse.getOp1();
+            int op2 = parse.getOp2();
+            char operator = parse.getOperator();
+            assertTrue(op1 == 2);
+            assertTrue(op2 == 2);
+            assertTrue(operator == '-');
+            } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testTwoMulTwo() {
+        Parse parse;
+        try {
+            parse = new Parse("2 * 2");
+            int op1 = parse.getOp1();
+            int op2 = parse.getOp2();
+            char operator = parse.getOperator();
+            assertTrue(op1 == 2);
+            assertTrue(op2 == 2);
+            assertTrue(operator == '*');
+            } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testTwoDivTwo() {
+        Parse parse;
+        try {
+            parse = new Parse("2 / 2");
+            int op1 = parse.getOp1();
+            int op2 = parse.getOp2();
+            char operator = parse.getOperator();
+            assertTrue(op1 == 2);
+            assertTrue(op2 == 2);
+            assertTrue(operator == '/');
+            } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
